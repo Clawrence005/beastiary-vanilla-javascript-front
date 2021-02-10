a71103f491a9a4ebb16fe062b11f9d8aa5b34d8d
colin <clawrence005@yahoo.com>
2021-02-10 01:26:00 -0600

    side nav open oncclick(w3 example) adding half circle

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("hello");
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+  document.getElementById(main).style.marginLeft = "250px";
+}
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
